0040e25dfe064fc74f438cb33689939d63a9b894
maze095 <maze89@mail.ru>
2024-12-11 23:20:21 +0300

    HelloWorldTest

---
+++ src/test/java/HelloTest.java
@@ -0,0 +1,14 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class HelloTest {
+
+    @Test
+    public void testHello(){
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/hello")
+                .andReturn();
+        response.prettyPrint();
+    }
+}
