949d9d72137c9a7569d87e183c0bba7263a58bb8
maze095 <maze89@mail.ru>
2024-12-22 17:01:24 +0300

    2. Создание простых запросов к API - Ex7: Долгий редирект

---
+++ src/test/java/testLongRedirect.java
@@ -0,0 +1,50 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class testLongRedirect {
+
+    @Test
+
+    public void longRedirect() {
+
+        /* Сначала выполняем запрос на апи эндпоинт */
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+
+        String locationHeader = response.getHeader("Location");
+        System.out.println("\nLocation: " + locationHeader);
+
+        int redirectCount = 0;
+        int statusCode = response.getStatusCode();
+
+        /* Затем выполняем запросы по переходу на URL в цикле, пока не получим код 200.
+        *  Также в конце цикла выводим URL для редиректа, который вернулся в ответе (если он есть) */
+        while (statusCode != 200) {
+
+              Response response2 = RestAssured
+                      .given()
+                      .redirects()
+                      .follow(false)
+                      .when()
+                      .get(locationHeader)
+                      .andReturn();
+
+              locationHeader = response2.getHeader("Location");
+              if (locationHeader != null) {
+                  System.out.println("\nLocation: " + locationHeader);
+              }
+
+              statusCode = response2.getStatusCode();
+              redirectCount++;
+
+          }
+
+        System.out.println("\nRedirects Count: " + redirectCount); // количество редиректов
+    }
+}
