70363860efd7954aef016098dbd3dc84d67b95a9
maze095 <maze89@mail.ru>
2025-01-06 17:29:49 +0300

    4. Создание фреймворка - Ex15: Тесты на метод user

--- src/test/java/tests/UserRegisterTest.java
+++ src/test/java/tests/UserRegisterTest.java
@@ -14,6 +14,8 @@ import  io.qameta.allure.Description;
 import  io.qameta.allure.Epic;
 import  io.qameta.allure.Feature;
 import  org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 @Epic("Registration cases")
 @Feature("Registration")
@@ -31,11 +33,11 @@ public class UserRegisterTest extends BaseTestCase {
         userData.put("email", email);
         userData = DataGenerator.getRegistrationData(userData);
 
-        Response responseCreateAuth = apiCoreRequests.makePostRequest(
+        Response responseCreateUser = apiCoreRequests.makePostRequest(
                 "https://playground.learnqa.ru/api/user/", userData);
 
-        Assertions.assertResponseCodeEquals(responseCreateAuth, 400);
-        Assertions.assertResponseTextEquals(responseCreateAuth, "Users with email '" + email + "' already exists");
+        Assertions.assertResponseCodeEquals(responseCreateUser, 400);
+        Assertions.assertResponseTextEquals(responseCreateUser, "Users with email '" + email + "' already exists");
     }
 
     @Test
@@ -45,10 +47,75 @@ public class UserRegisterTest extends BaseTestCase {
 
         Map<String, String> userData = DataGenerator.getRegistrationData();
 
-        Response responseCreateAuth = apiCoreRequests.makePostRequest(
+        Response responseCreateUser = apiCoreRequests.makePostRequest(
                 "https://playground.learnqa.ru/api/user/", userData);
 
-        Assertions.assertResponseCodeEquals(responseCreateAuth, 200);
-        Assertions.assertJsonHasField(responseCreateAuth, "id");
+        Assertions.assertResponseCodeEquals(responseCreateUser, 200);
+        Assertions.assertJsonHasField(responseCreateUser, "id");
+    }
+
+    @Test
+    @Description("This test tries to register a user with invalid email")
+    @DisplayName("Test create user with invalid email negative")
+    public void testCreateUserWithInvalidEmailNegative() {
+        String invalidEmail = "borisbritva1example.com"; // отсутствует символ @
+
+        Map<String, String> userData = new HashMap<>();
+        userData.put("email", invalidEmail);
+        userData = DataGenerator.getRegistrationData(userData);
+
+        Response responseCreateUser = apiCoreRequests.makePostRequest(
+                "https://playground.learnqa.ru/api/user/", userData);
+
+        Assertions.assertResponseCodeEquals(responseCreateUser, 400);
+        Assertions.assertResponseTextEquals(responseCreateUser, "Invalid email format");
+    }
+
+    @Description("This test tries to register a user without one of the required fields")
+    @DisplayName("Test create user without required fields negative")
+    @ParameterizedTest
+    @ValueSource(strings = {"username", "firstName", "lastName", "email", "password"})
+    public void testCreateUserMissedRequiredFieldsNegative(String field) {
+        Map<String, String> userData = new HashMap<>();
+        userData.put(field, null);
+        userData = DataGenerator.getRegistrationData(userData);
+
+        Response responseCreateUser = apiCoreRequests.makePostRequest(
+                "https://playground.learnqa.ru/api/user/", userData);
+
+        Assertions.assertResponseCodeEquals(responseCreateUser, 400);
+        Assertions.assertResponseTextEquals(responseCreateUser, "The following required params are missed: " + field);
+    }
+
+    @Description("This test tries to register a user with too short name")
+    @DisplayName("Test create user with too short name")
+    @ParameterizedTest
+    @ValueSource(strings = {"username", "firstName", "lastName"})
+    public void testCreateUserWithTooShortNameNegative(String field) {
+        Map<String, String> userData = new HashMap<>();
+        userData.put(field, "A");
+        userData = DataGenerator.getRegistrationData(userData);
+
+        Response responseCreateUser = apiCoreRequests.makePostRequest(
+                "https://playground.learnqa.ru/api/user/", userData);
+
+        Assertions.assertResponseCodeEquals(responseCreateUser, 400);
+        Assertions.assertResponseTextEquals(responseCreateUser, "The value of '" + field + "' field is too short");
+    }
+
+    @Description("This test tries to register a user with too long name")
+    @DisplayName("Test create user with too long name")
+    @ParameterizedTest
+    @ValueSource(strings = {"username", "firstName", "lastName"})
+    public void testCreateUserWithTooLongNameNegative(String field) {
+        Map<String, String> userData = new HashMap<>();
+        userData.put(field, "XYoi06sBu4qUkNYTugISI7Xi1YiMx0NlwzCGtpuMDRlbZhsk8wbUoNwfC6ogJKuJY2uD3YclsnZrBFXaf7kCoWb6qGRwjO90rw6quvrqJPTId6lMmr9X6DvqmYI9T0muRrnjPl6RNYMdzAjKljwpXsGmJUSTFkiUpTH0ZIjT312afK9ypkbkQNoihUGaYMNTDI0XxwodGXwLXgl8sILmsnjlWdj3jXLYRz9fGPrY8dy5hiR1CpARN4D6kaT2TDJ");
+        userData = DataGenerator.getRegistrationData(userData);
+
+        Response responseCreateUser = apiCoreRequests.makePostRequest(
+                "https://playground.learnqa.ru/api/user/", userData);
+
+        Assertions.assertResponseCodeEquals(responseCreateUser, 400);
+        Assertions.assertResponseTextEquals(responseCreateUser, "The value of '" + field + "' field is too long");
     }
 }
