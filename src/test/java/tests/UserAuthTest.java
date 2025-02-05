c952e1f6b66c537e4daa6ab2fafce686cb2fa71e
Ivan Pechenkin <maze89@mail.ru>
2025-02-05 19:20:42 +0300

    Добавлены дополнительные тесты на авторизацию

--- src/test/java/tests/UserAuthTest.java
+++ src/test/java/tests/UserAuthTest.java
@@ -51,14 +51,41 @@ public class UserAuthTest extends BaseTestCase {
         Assertions.assertJsonByName(responseCheckAuth, "user_id", this.userIdOnAuth);
     }
 
-    @ParameterizedTest (name = "Test auth user negative. Auth with {0} only")
+    @ParameterizedTest (name = "Test auth user negative. Auth with invalid {0}")
     @ValueSource(strings = {"cookie", "header"})
-    @Description("This test checks authorization status without sending auth cookie or token ")
-    @DisplayName("Test auth user negative")
+    @Description("This test checks authorization status by sending invalid auth cookie or token ")
+    @DisplayName("Test auth user with invalid auth data")
     @Story("Negative tests")
     @Tags({@Tag("api"),@Tag("smoke"),@Tag("user")})
     @Owner("Ivan Pechenkin")
-    public void testNegativeAuthUser(String condition){
+    public void testAuthUserWithInvalidAuthData(String condition){
+
+        if (condition.equals("cookie")) {
+            Response responseForCheck = apiCoreRequests
+                    .makeGetRequest(ENVIRONMENT + "user/auth",
+                            this.header,
+                            "123");
+            Assertions.assertJsonByName(responseForCheck, "user_id", 0);
+
+        } else if (condition.equals("header")) {
+            Response responseForCheck = apiCoreRequests
+                    .makeGetRequest(ENVIRONMENT + "user/auth",
+                            "123",
+                            this.cookie);
+            Assertions.assertJsonByName(responseForCheck, "user_id", 0);
+        } else {
+            throw new IllegalArgumentException("Condition value is unknown " + condition);
+        }
+    }
+
+    @ParameterizedTest (name = "Test auth user negative. Auth with {0} only")
+    @ValueSource(strings = {"cookie", "header"})
+    @Description("This test checks authorization status without sending auth cookie or token ")
+    @DisplayName("Test auth user without auth data")
+    @Story("Negative tests")
+    @Tags({@Tag("api"),@Tag("smoke"),@Tag("user")})
+    @Owner("Ivan Pechenkin")
+    public void testAuthUserMissedAuthData(String condition){
 
         if (condition.equals("cookie")) {
             Response responseForCheck = apiCoreRequests.makeGetRequestWithCookie(
@@ -68,7 +95,7 @@ public class UserAuthTest extends BaseTestCase {
             Assertions.assertJsonByName(responseForCheck, "user_id", 0);
         } else if (condition.equals("header")) {
             Response responseForCheck = apiCoreRequests.makeGetRequestWithToken(
-                    "https://playground.learnqa.ru/api/user/auth",
+                    ENVIRONMENT + "user/auth",
                     this.header
             );
             Assertions.assertJsonByName(responseForCheck, "user_id", 0);
